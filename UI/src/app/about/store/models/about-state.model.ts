a2d5d4c4f37b0d0d82fca04793066e3f8630ebbb
Jeric Alombro <jerichalombro@email.com>
2026-04-05 23:10:32 +0800

    remove about page

--- UI/src/app/about/store/models/about-state.model.ts
+++
@@ -1,7 +0,0 @@
-import { TeamMember } from './team-member.model';
-
-export interface AboutState {
-  team: TeamMember[];
-  loading: boolean;
-  error: string | null;
-}
--- UI/src/app/about/store/models/team-member.model.ts
+++
@@ -1,6 +0,0 @@
-export interface TeamMember {
-  id: string;
-  name: string;
-  role: string;
-  bio: string;
-}
